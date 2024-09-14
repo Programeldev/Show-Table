4653d6e3c9c65f6e2f687a450a2d5b7fb15cad68
Programeldev <wojtek.deren@spoko.pl>
2024-09-14 19:00:44 +0200

    Added animation in configuration page.

--- src/globals.py
+++ src/globals.py
@@ -1,2 +1,3 @@
 config_entries: dict[str, str] = dict()
+config_entries_keys = ['host', 'database', 'user', 'password']
 logs: str = str()
--- src/pages.py
+++ src/pages.py
@@ -78,11 +78,14 @@ class TablePage(Gtk.Box):
 
 
 class ConfigurationPage(Gtk.Grid):
-    entries: dict[str, Gtk.Entry()]
+    entries_revealers: dict[str, Gtk.Entry()]
     entries_label: dict[str, Gtk.Label()]
 
     def __init__(self):
         super().__init__()
+
+        DURATION = 200
+        ANIMATION = Gtk.RevealerTransitionType.SWING_LEFT
 
         setCssStyleForWidget(
             self,
@@ -94,24 +97,37 @@ class ConfigurationPage(Gtk.Grid):
                 }
             ''')
 
-        self.entries = {
-            'host':     Gtk.Entry(text='localhost'),
-            'user':     Gtk.Entry(text='root'),
-            'password': Gtk.Entry(),
-            'database': Gtk.Entry()
+        self.entries = [
+            Gtk.Entry(text='localhost'),
+            Gtk.Entry(text='root'),
+            Gtk.Entry(),
+            Gtk.Entry()
+        ]
+
+        revealers = {
+            'host':     Gtk.Revealer(),
+            'user':     Gtk.Revealer(),
+            'password': Gtk.Revealer(),
+            'database': Gtk.Revealer()
         }
 
+        # set default input text in entries
         self.__saveConfigEntries(None)
 
         entries_label = dict()
 
-        for entry in self.entries.values():
+        for entry, revealer in zip(self.entries, revealers.values()):
             entry.set_hexpand(True)
             entry.set_vexpand(False)
             entry.connect('unmap', self.__saveConfigEntries)
+            revealer.set_child(entry)
+            revealer.set_transition_duration(DURATION)
+            revealer.set_transition_type(ANIMATION)
+            revealer.connect('unmap', self.__reveal, False)
+            revealer.connect('map', self.__reveal, True)
 
-        # Hide entered password input
-        self.entries['password'].set_visibility(False)
+        # Hide entered input password
+        self.entries[2].set_visibility(False)
 
         entries_label_text = [
             'IP Address',
@@ -134,48 +150,22 @@ class ConfigurationPage(Gtk.Grid):
                 }
                 '''
             )
-            entries_label.update({label_text: label})
+            revealer = Gtk.Revealer()
+            revealer.set_child(label)
+            revealer.set_transition_duration(DURATION)
+            revealer.set_transition_type(ANIMATION)
+            revealer.connect('unmap', self.__reveal, False)
+            revealer.connect('map', self.__reveal, True)
+            entries_label.update({label_text: revealer})
 
         self.attach(entries_label['IP Address'], 0, 0, 1, 1)
-        self.attach(self.entries['host'], 1, 0, 2, 1)
+        self.attach(revealers['host'], 1, 0, 2, 1)
         self.attach(entries_label['Database Name'], 0, 1, 1, 1)
-        self.attach(self.entries['database'], 1, 1, 2, 1)
+        self.attach(revealers['database'], 1, 1, 2, 1)
         self.attach(entries_label['Username'], 0, 2, 1, 1)
-        self.attach(self.entries['user'], 1, 2, 2, 1)
+        self.attach(revealers['user'], 1, 2, 2, 1)
         self.attach(entries_label['Password'], 0, 3, 1, 1)
-        self.attach(self.entries['password'], 1, 3, 2, 1)
-
-        save_button = Gtk.Button(label='   Save   ')
-        save_button.set_halign(Gtk.Align.CENTER)
-        save_button.set_hexpand(True)
-        setCssStyleForWidget(
-            save_button,
-            b'''
-            button
-            {
-                margin: 60px 100px 0px 100px;
-            }
-            '''
-        )
-        # save_button.connect('clicked', self.__saveConfigEntries)
-        # setCssStyleForWidget(
-        #     save_button,
-        #     b'''
-        #     button
-        #     {
-        #         font-size: 20pt;
-        #         border: 50px;
-        #     }
-        #     '''
-        # )
-
-        # self.attach_next_to(
-        #     save_button,
-        #     self.entries['user'],
-        #     Gtk.PositionType.BOTTOM,
-        #     2,
-        #     1
-        # )
+        self.attach(revealers['password'], 1, 3, 2, 1)
 
         self.set_row_spacing(10)
         self.set_column_spacing(10)
@@ -183,5 +173,9 @@ class ConfigurationPage(Gtk.Grid):
     def __saveConfigEntries(self, button):
         globals.config_entries = {}
 
-        for key, value in self.entries.items():
-            globals.config_entries.update({key: value.get_text()})
+        for entry, key in zip(self.entries,
+                              globals.config_entries_keys):
+            globals.config_entries.update({key: entry.get_text()})
+
+    def __reveal(self, revealer, show):
+        revealer.set_reveal_child(show)
